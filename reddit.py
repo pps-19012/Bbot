27ff4bb85f70e11787b3a54bbdcc30702f673636
Pushpendra Pratap Singh <76405318+pps-19012@users.noreply.github.com>
2022-06-26 16:12:48 +0530

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -16,6 +16,7 @@ def cryptocurrency():
       if i==random_post_number:
           value = post.title + "\n" + post.url
           return value
+
 def bnp():
   sub1 = reddit.subreddit("BeautifulNaturePhotos")
   top1 = sub1.top(limit = 150)
@@ -23,4 +24,130 @@ def bnp():
   for i,post in enumerate(top1):
       if i==random_post_number:
           value = post.url
+          return value
+
+def memes():
+  sub1 = reddit.subreddit("memes")
+  hot1 = sub1.hot(limit = 150)
+  random_post_number = random.randint(0,150)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.url
+          return value
+
+def dankmemes():
+  sub1 = reddit.subreddit("dankmemes")
+  hot1 = sub1.hot(limit = 150)
+  random_post_number = random.randint(0,150)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.url
+          return value
+
+def redditannounce():
+  sub1 = reddit.subreddit("announcements")
+  hot1 = sub1.hot(limit = 150)
+  random_post_number = random.randint(0,150)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.url
+          return value
+
+def worldnews():
+  sub1 = reddit.subreddit("worldnews")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def gaming():
+  sub1 = reddit.subreddit("gaming")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def music():
+  sub1 = reddit.subreddit("Music")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def movies():
+  sub1 = reddit.subreddit("movies")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def news():
+  sub1 = reddit.subreddit("news")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def gifs():
+  sub1 = reddit.subreddit("gifs")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def books():
+  sub1 = reddit.subreddit("books")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def sports():
+  sub1 = reddit.subreddit("sports")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def showerthoughts():
+  sub1 = reddit.subreddit("Showerthoughts")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def earth():
+  sub1 = reddit.subreddit("EarthPorn")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
+          return value
+
+def jokes():
+  sub1 = reddit.subreddit("Jokes")
+  hot1 = sub1.hot(limit = 100)
+  random_post_number = random.randint(0,100)
+  for i,post in enumerate(hot1):
+      if i==random_post_number:
+          value = post.title + "\n" + post.url
           return value
