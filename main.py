dbb82222a12db1e1e9dd6d3c3f57ac4d76f16f45
Pushpendra Pratap Singh <76405318+pps-19012@users.noreply.github.com>
2022-06-26 16:12:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def on_message(msg):
 
   if(msg.content.startswith("//help")):
     # print('received')
-    await msg.channel.send('commands - //hello, //svquote, //ramquote, //cryptocurrency, //bnp')
+    await msg.channel.send('commands - //hello, //svquote, //ramquote, //cryptocurrency, //bnp, //memes, //dankmemes, //redditannounce, //worldnews, //gaming, //music, //movies, //news, //gif, //books, //sports, //showerthoughts, //earth, //jokes')
 
   if(msg.content.startswith("//svquote")):
     txt = scraping.svquote()
@@ -40,10 +40,66 @@ async def on_message(msg):
   if(msg.content.startswith("//cryptocurrency")):
     txt = reddit.cryptocurrency()
     await msg.channel.send(txt)
-    
+
   if(msg.content.startswith("//bnp")):
     txt = reddit.bnp()
     await msg.channel.send(txt)
 
+  if(msg.content.startswith("//memes")):
+    txt = reddit.memes()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//dankmemes")):
+    txt = reddit.dankmemes()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//redditannounce")):
+    txt = reddit.redditannounce()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//worldnews")):
+    txt = reddit.worldnews()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//gaming")):
+    txt = reddit.gaming()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//music")):
+    txt = reddit.music()
+    await msg.channel.send(txt)
+    
+  if(msg.content.startswith("//movies")):
+    txt = reddit.movies()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//news")):
+    txt = reddit.news()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//gif")):
+    txt = reddit.gifs()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//books")):
+    txt = reddit.books()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//sports")):
+    txt = reddit.sports()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//showerthoughts")):
+    txt = reddit.showerthoughts()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//earth")):
+    txt = reddit.earth()
+    await msg.channel.send(txt)
+
+  if(msg.content.startswith("//jokes")):
+    txt = reddit.jokes()
+    await msg.channel.send(txt)
+    
 keep_alive()
 client.run(os.environ['TOKEN'])
